0c26e2060a386c9dfdfb824a87a43e6858846115
dsdhumale <d.sdhumale@gmail.com>
2022-11-17 08:23:48 +0530

    Added : Array functions

---
+++ Array.js
@@ -0,0 +1,50 @@
+// // Array.from()
+// const arr=Array.from("Devendra")
+// console.log(arr);
+
+// //Adding array element at last Arry.push
+// arr.push("Devendra")
+// console.log(arr);
+// arr.push(true)
+// console.log(arr);
+
+// //remove array element at last Arry.pop
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// //length of array
+// console.log(arr.length);
+
+// //Array.shift--- remove from start
+// arr.shift();
+// console.log(arr);
+
+// //Array.unshift----add at start
+// arr.unshift("D");
+// console.log(arr);
+
+//array.forEach() function
+// function myFunction(){
+//     const element=[25,13,22];
+//     const add=[];
+//     element.forEach(function (element){
+//         add.push(element + 6);
+//     });
+//     console.log(add);
+// }
+// myFunction();
+
+//Array.filter
+// const num=[25,30,14,48]
+// function first(age){
+//     return age >=18;
+// }
+//  function second(){
+//      let filter=num.filter(first);
+//      console.log(filter);
+//  }
+// second();
+// let filter=num.filter(first);
+//  console.log(filter);
